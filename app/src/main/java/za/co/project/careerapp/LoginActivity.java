815cc15c83202adf880543116a99cec401aa9d87
Ryshan Ramlall <ryshanramlall@outlook.com>
2023-05-10 21:37:49 +0200

    Added: LoginActivity, no code working

---
+++ app/src/main/java/za/co/project/careerapp/LoginActivity.java
@@ -0,0 +1,14 @@
+package za.co.project.careerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
